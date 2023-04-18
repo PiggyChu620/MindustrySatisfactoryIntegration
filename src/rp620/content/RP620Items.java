9497ecf099e67e99b068d4670ec6ef0097ec8d3e
PiggyChu620 <ytchu620@gmail.com>
2023-04-18 18:35:18 +0800

    202304181835

--- src/rp620/content/RP620Items.java
+++ src/rp620/content/RP620Items.java
@@ -17,29 +17,35 @@ public class RP620Items
         sulfur=new Item("sulfur", Color.yellow)
         {{
             localizedName="Sulfur";
+            alwaysUnlocked=true;
         }};
         salt=new Item("salt", Color.white)
         {{
             localizedName="Salt";
             Blocks.salt.itemDrop=this;
+            alwaysUnlocked=true;
         }};
         lye=new Item("lye",Color.white)
         {{
             localizedName="Lye";
+            alwaysUnlocked=true;
         }};
         soap=new Item("soap", Color.pink)
         {{
             localizedName="Soap";
+            alwaysUnlocked=true;
         }};
         celluloseFiber=new Item("cellulose-fiber", Color.lightGray)
         {{
             localizedName="Cellulose Fiber";
+            alwaysUnlocked=true;
         }};
         dynamite=new Item("dynamite",Color.red)
         {{
             localizedName="Dynamite";
             flammability=10;
             explosiveness=2;
+            alwaysUnlocked=true;
         }};
         onc=new Item("onc", Color.sky)
         {{
@@ -47,6 +53,7 @@ public class RP620Items
             description="Octanitrocubane\nThe most explosive compound to date that had successfully been synthesized (by Philip Eaton).";
             flammability=10;
             explosiveness=2.38f;
+            alwaysUnlocked=true;
         }};
         azidoazideAzide=new Item("azidoazide-azide", Color.valueOf("fdfd96"))
         {{
@@ -54,6 +61,7 @@ public class RP620Items
             description="The most unstable compound known to man, it's so unstable that it'll explode when studying it, some believe that it's even more explosive than ONC.";
             flammability=10;
             explosiveness=3;
+            alwaysUnlocked=true;
         }};
         oac=new Item("oac",Color.red)
         {{
@@ -61,6 +69,7 @@ public class RP620Items
             description="Octaazacubane\nThe most ever explosive hypothetical compound, it's 8 nitrogen atoms arranged into a perfect cube, it's predicted to be over 5 times more powerful than standard TNT.";
             flammability=10;
             explosiveness=5+ 5 * new Rand(LocalDateTime.now().getSecond()).nextFloat();
+            alwaysUnlocked=true;
         }};
     }
 }
--- src/rp620/content/RP620Liquids.java
+++ src/rp620/content/RP620Liquids.java
@@ -16,6 +16,7 @@ public class RP620Liquids
         {{
            localizedName="Nitrogen Oxide";
            gas=true;
+            alwaysUnlocked=true;
            //TBD
         }};
         nitrogenDioxide=new Liquid("nitrogen-dioxide", Color.valueOf("A52A2A"))
@@ -25,11 +26,13 @@ public class RP620Liquids
             heatCapacity=0;
             coolant=false;
             boilPoint=0;
+            alwaysUnlocked=true;
         }};
         ammonia=new Liquid("ammonia", Color.valueOf("0073CF"))
         {{
             localizedName="Ammonia";
             gas=true;
+            alwaysUnlocked=true;
             //TBD
         }};
         glycerin=new Liquid("glycerin",Color.valueOf("66CCCC"))
@@ -38,6 +41,7 @@ public class RP620Liquids
             viscosity=0.75f;
             explosiveness=0.75f;
             coolant=false;
+            alwaysUnlocked=true;
         }};
         nitricAcid=new Liquid("nitric-acid",Color.valueOf("ffff00"))
         {{
@@ -45,6 +49,7 @@ public class RP620Liquids
             heatCapacity=0.14f;
             viscosity=0.6f;
             coolant=false;
+            alwaysUnlocked=true;
         }};
         nitroglycerin=new Liquid("nitroglycerin", Color.valueOf("ff3f3f"))
         {{
@@ -55,12 +60,14 @@ public class RP620Liquids
             explosiveness=1.54f;
             coolant=false;
             effect= StatusEffects.blasted;
+            alwaysUnlocked=true;
         }};
         hydrazine=new Liquid("hydrazine",Color.purple)
         {{
             localizedName="Hydrazine";
             flammability=5;
             explosiveness=.5f;
+            alwaysUnlocked=true;
         }};
     }
 }
